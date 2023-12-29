520c0e82bda9a555b05bf6ced1641543fc318838
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2023-12-29 23:31:17 +0200

    Complete simple footer

---
+++ resources/views/livewire/dashboard/partials/footer.blade.php
@@ -0,0 +1,23 @@
+<footer class="mt-4 footer">
+    <div class="container">
+        <div class="row align-items-center ">
+            <div class="col-12 col-md-6">
+                <p>Design with ❤️ by <a href="https://github.com/ahmedelsewailky" target="_blank">Ahmed Elsewailky</a>, Rights resaved © {{ date('Y') }}</p>
+            </div>
+
+            <div class="col-12 col-md-6">
+                <ul class="nav float-end ">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">{{ __('Home') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">{{ __('About') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">{{ __('Documentation') }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
