ce2b9637b4c040b38dddd3a9f13e89b2f6c065ff
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2023-12-29 23:37:20 +0200

    Init project

---
+++ app/Providers/VoltServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Livewire\Volt\Volt;
+
+class VoltServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Volt::mount([
+            resource_path('views/livewire'),
+            resource_path('views/livewire/dashboard'),
+            resource_path('views/livewire/dashboard/pages'),
+        ]);
+    }
+}
